0a52b515c0c5f4bc22d15f4db153cb27706953ef
jastakpawel <innyjastak@gmail.com>
2017-05-29 14:16:01 +0200

    first commit

---
+++ scripts.js
@@ -0,0 +1,5 @@
+var a = prompt('Wpisz długość podstawy');
+var h = prompt('Wpisz wysokość trójkąta');
+var areaOfTriangle = a*h/2;
+alert('Pole twojego trójkąta wynosi: ' + areaOfTriangle);
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
